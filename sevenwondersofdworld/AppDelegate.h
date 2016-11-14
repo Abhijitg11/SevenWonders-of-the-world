dc6b379e97948dace8478a4e2117980848b66ec5
Student <Student@apples-MacBook-Air-3.local>
2016-11-14 09:04:55 +0530

    Initial Commit

---
+++ sevenwondersofdworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sevenwondersofdworld
+//
+//  Created by Student on 14/11/16.
+//  Copyright (c) 2016 Abhijit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sevenwondersofdworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sevenwondersofdworld
+//
+//  Created by Student on 14/11/16.
+//  Copyright (c) 2016 Abhijit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
